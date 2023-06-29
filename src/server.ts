a736cc332336a8466fff29cb874fb2853957184d
ingridlorrane <sql.ingrid@gmail.com>
2023-06-29 02:05:37 -0300

    crud

--- src/server.ts
+++ src/server.ts
@@ -34,8 +34,8 @@ app.get('/modality', getModality);
 app.get('/students', getStudents);
 
 // rotas POST
-app.post('/users/:id', addUser);
-app.post('/students/:id', addStudents);
+app.post('/users', addUser);
+app.post('/students', addStudents);
 
 // rotas PUT
 app.put('/users/:id', updateUsers);
